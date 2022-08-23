1978b76e506b018231a7c1000150c2ce01d6b243
Nik Lyons <nikolas.lyons23@gmail.com>
2022-08-23 11:52:40 -0600

    we are pushing zach

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
+
 export default class Drinks extends Component {
   render() {
     return (
-      <div className="drinks-container">
+      
+
+<div className="gallery style2 medium lightbox onscroll-fade-in">
 								<article>
-									<a href="images/SiteMock.png" className="image">
-										<img className="project-img" src="https://images.unsplash.com/photo-1513188732907-5f732b831ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80" alt="" />
+									<a href="src/assets/Images/bubble-gum-colada.webp" className="image">
+										<img className="project-img" src="https://tipsybartender.com/wp-content/uploads/2018/02/bubble-gum-colada.jpg" alt="" />
 									</a>
 									<div className="caption">
 										<h3>SiteMock</h3>
@@ -18,7 +21,7 @@ export default class Drinks extends Component {
 								</article>
 								<article>
 									<a href="images/wayfinder.png" className="image">
-										<img className="project-img" src="https://www.pngall.com/wp-content/uploads/5/Sail-PNG.png" alt="" />
+										<img className="project-img" src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/06/sex-on-the-beach-cocktail-recipe.jpg" alt="" />
 									</a>
 									<div className="caption">
 										<h3>Wayfinder</h3>
@@ -30,7 +33,7 @@ export default class Drinks extends Component {
 								</article>
 								<article>
 									<a href="images/Keepr.png" className="image">
-										<img className="project-img" src="https://www.pngplay.com/wp-content/uploads/1/Letter-K-PNG-Stock-Photo.png" alt="" />
+										<img className="project-img" src="https://images.immediate.co.uk/production/volatile/sites/2/2021/07/GettyImages_933801552-6e4a61e.jpg?quality=90&resize=700,466" alt="" />
 									</a>
 									<div className="caption">
 										<h3>KeepR</h3>
@@ -42,7 +45,7 @@ export default class Drinks extends Component {
 								</article>
 								<article>
 									<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-										<img className="project-img" src="https://www.nicepng.com/png/full/313-3132439_b-b-png.png" alt="" />
+										<img className="project-img" src="https://insanelygoodrecipes.com/wp-content/uploads/2021/09/Cold-Strawberry-Cocktail-with-Fresh-Strawberries-and-Mint.jpg" alt="" />
 									</a>
 									<div className="caption">
 										<h3>BnBooking</h3>
@@ -54,7 +57,7 @@ export default class Drinks extends Component {
 								</article>
 								<article>
 									<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-										<img className="project-img" src="https://www.nicepng.com/png/full/313-3132439_b-b-png.png" alt="" />
+										<img className="project-img" src="https://hips.hearstapps.com/del.h-cdn.co/assets/15/38/1442432590-delish-halloween-cocktails-sweet-poison.png" alt="" />
 									</a>
 									<div className="caption">
 										<h3>BnBooking</h3>
@@ -66,7 +69,7 @@ export default class Drinks extends Component {
 								</article>
 								<article>
 									<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-										<img className="project-img" src="https://www.nicepng.com/png/full/313-3132439_b-b-png.png" alt="" />
+										<img className="project-img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-191017-brunch-punch-0210-landscape-pf-1649601124.jpg?crop=0.673xw:1.00xh;0.228xw,0&resize=640:*" alt="" />
 									</a>
 									<div className="caption">
 										<h3>BnBooking</h3>
@@ -78,7 +81,7 @@ export default class Drinks extends Component {
 								</article>
 								<article>
 									<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-										<img className="project-img" src="https://www.nicepng.com/png/full/313-3132439_b-b-png.png" alt="" />
+										<img className="project-img" src="https://recipetineats.com/wp-content/uploads/2019/10/Cosmopolitan-cocktail_2.jpg" alt="" />
 									</a>
 									<div className="caption">
 										<h3>BnBooking</h3>
@@ -90,7 +93,7 @@ export default class Drinks extends Component {
 								</article>
 								<article>
 									<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-										<img className="project-img" src="https://www.nicepng.com/png/full/313-3132439_b-b-png.png" alt="" />
+										<img className="project-img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/w-punta-de-mita-piwiwi-cocktail-preview-1548176063.jpeg?crop=1xw:1xh;center,top&resize=480:*" alt="" />
 									</a>
 									<div className="caption">
 										<h3>BnBooking</h3>
@@ -102,7 +105,7 @@ export default class Drinks extends Component {
 								</article>
 								<article>
 									<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-										<img className="project-img" src="https://www.nicepng.com/png/full/313-3132439_b-b-png.png" alt="" />
+										<img className="project-img" src="https://www.acouplecooks.com/wp-content/uploads/2020/03/Margarita-024.jpg" alt="" />
 									</a>
 									<div className="caption">
 										<h3>BnBooking</h3>
@@ -113,6 +116,8 @@ export default class Drinks extends Component {
 									</div>
 								</article>
 							</div>
+
+		
     )
   }
 }
