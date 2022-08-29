cad296d0c1ad8559ca9ae36e063439b0e94464f4
Nik Lyons <nikolas.lyons23@gmail.com>
2022-08-29 14:01:18 -0600

    Real Data placed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import smallPlatter from '../assets/Images/1-2-person-platter.png'
+import largePlatter from '../assets/Images/4-7-person-platter.png'
 
 export default class Menu extends Component {
   render() {
     return (
+      
       <div className='menu row m-3'>
+        <div className='col-12 d-flex justify-content-around'>
+          <h1 className='border-bottom border-dark'>Drinks</h1>
+          <h1 className='border-bottom border-dark'>Food</h1>
+        </div>
         <div className='col-6 p-3 border border-dark rounded'>
           <h2>
             Cocktails
@@ -45,20 +52,55 @@ export default class Menu extends Component {
         <div className='col-6 p-3'>
           <div className='row border border-dark rounded p-3 m-2'>
             <div className='col-6'>
-              <p>this is a description</p>
+              <div className='row'>
+                <div className='col-12'>
+
+              <h3>Option 1 Classic</h3>
+              <p>
+                    This board comes with Salami, brie bites and manchego
+                    cheese. Seasonal fruit and veggies with a variety of nuts,
+                    crackers, olives, cornichon pickles and a sweet treat will
+                    pair nicely together. (May come with substitutions)
+              </p>
+                    <h3>Option 2. Vegetarian</h3>
+                    <p>This board will come with 2 different cheeses, brie and
+                    manchego. Crackers, nuts and a variety of Olives and
+                    pickles. The board also comes with a dip to accompany the seasonal veggies and fruit.</p>
+                    <p>2-4 people<span>$45/$55</span></p>
+              </div>
+                </div>
             </div>
             <div className='col-6'>
-              <img  className='img-fluid charcuterie-board-img' src='https://placekitten.com/400' alt=''/>
+              <img  className='img-fluid charcuterie-board-img rounded' src={smallPlatter} alt=''/>
             </div>
 
           </div>
           <div className='row border border-dark rounded p-3 m-2'>
             <div className='col-6'>
               
-              <img  className='img-fluid charcuterie-board-img' src='https://placekitten.com/400' alt=''/>
+              <img  className='img-fluid charcuterie-board-img rounded' src={largePlatter} alt=''/>
             </div>
             <div className='col-6'>
-            <p>this is a description</p>
+              <div className='row'>
+                <div className='col-12'>
+                  <h3>Option 1 Classic</h3>
+                  <p> 
+                    Salami and prosciutto along with a brie round and a
+                    Syrah soaked bellavittano cheese will accompany this
+                    board. Crackers and a variety of nuts and seasonal fruit
+                    will compliment and pair with the meat and cheeses.
+                    Olives and a small jar of honey will be the perfect taste
+                    to add to any of the cheeses. (May come with
+                    substitutions)
+                      </p>
+                    <h3>Option 2 Vegetarian</h3>
+                    <p>This vegetarian board will come with 3 different
+                    cheeses. Brie, manchego and a Syrah soaked</p>
+
+                    <p>4-7 people<span>$45/$55</span></p>
+                </div>
+
+              </div>
             </div>
 
           </div>
