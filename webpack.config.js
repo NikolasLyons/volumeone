78049572f678554bd86377e8c698b9d8b73b666e
Nik Lyons <nikolas.lyons23@gmail.com>
2022-09-09 22:48:24 -0600

    adds webpack config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|gif|png|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ],
+  }
+}
