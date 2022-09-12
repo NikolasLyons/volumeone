38d2a160f6505564571ce3cad4c9c5c3cd8f7b46
Nik Lyons <nikolas.lyons23@gmail.com>
2022-09-12 11:42:01 -0600

    changed to menu

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import logo from '../assets/Images/volumeone2.jpg'
+
 
 
 export default function Banner() {
   return (
-    <div className=' col-12 bannerBack' style={{backgroundImage: `url("http://cdn.cnn.com/cnnnext/dam/assets/191202161925-budapest-bars---parisi-udvar-hotel-budapest-parisi-passage-bar.jpg")`}}>
+    <div className=' col-12 bannerBack' style={{ backgroundImage: `url("http://cdn.cnn.com/cnnnext/dam/assets/191202161925-budapest-bars---parisi-udvar-hotel-budapest-parisi-passage-bar.jpg")` }}>
       <div className='text-center p-4 text-light'>
         <h1>NOW OPEN!</h1>
         <h3>See Hours Below!</h3>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,40 +1,28 @@
 import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
-import Drinks from './Drinks'
-import Food from './Food'
 
 
-export default function Menu()  {
-  const [showDrinks, setShowDrinks] = useState(true)
-  const [showFood, setShowFood] = useState(false)
-  
+
+export default function Menu() {
 
 
 
 
-  const handleShowDrinks = () => {
-    console.log(showDrinks)
-    console.log(showFood)
-    return setShowDrinks( showDrinks === true) 
-  }
-  const handleShowFood = () => {
-    console.log(showFood)
-    console.log(showDrinks)
-    return setShowFood(showFood === true) 
-    
-     }
-  
-    return (
+
+
+
+
+  return (
     <div>
       <div className='col-12 d-flex justify-content-around p-4'>
-      <Link to="/menu/drinks" className='tabs'><h1>Drinks</h1></Link>
-      <Link to="/menu/food" className='tabs'><h1>Food</h1></Link>
+        <Link to="/menu/drinks" className='tabs'><h1>Drinks</h1></Link>
+        <Link to="/menu/food" className='tabs'><h1>Food</h1></Link>
+      </div>
+      <Outlet />
     </div>
-     <Outlet />
-    </div>
-        
-      
-    )
-    }
+
+
+  )
+}
 
