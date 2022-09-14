1d3a0b7a02c8034602a2db43af31b4f25d07571d
Nik Lyons <nikolas.lyons23@gmail.com>
2022-09-14 13:19:34 -0600

    push

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,114 +1,116 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 
 
-export default class Gallery extends Component {
-  render() {
-    return (
+export default function Gallery() {
+
+
+
+	return (
 		<div className='p-3'>
 			<div className='text-center'>
 				<h2>Gallery</h2>
 			</div>
 			<div className="gallery style2 medium lightbox onscroll-fade-in">
-	
-	
-	<article className='p-2'>
-		<a href="src/assets/Images/bubble-gum-colada.webp" className="image">
-			<img className="project-img rounded" src="https://tipsybartender.com/wp-content/uploads/2018/02/bubble-gum-colada.jpg" alt="" />
-		</a>
-		<div className="caption">
-			<h3>Bubble Gum Colada</h3>
-			<p>Taste the exciting flavors of this Cotton Candy drink</p>
-		</div>
-	</article>
-	<article className='p-2'>
-		<a href="images/wayfinder.png" className="image">
-			<img className="project-img rounded" src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/06/sex-on-the-beach-cocktail-recipe.jpg" alt="" />
-		</a>
-		<div className="caption">
-			<h3>Sex on The Beach</h3>
-			<p>No Description needed</p>
-			
-		</div>
-	</article>
-	<article className='p-2'>
-		<a href="images/Keepr.png" className="image">
-			<img className="project-img rounded" src="https://images.immediate.co.uk/production/volatile/sites/2/2021/07/GettyImages_933801552-6e4a61e.jpg?quality=90&resize=700,466" alt="" />
-		</a>
-		<div className="caption">
-			<h3>Grapefruit Fresca</h3>
-			<p>All Strawberry fun</p>
-			
-		</div>
-	</article>
-	<article className='p-2'>
-		<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-			<img className="project-img rounded" src="https://insanelygoodrecipes.com/wp-content/uploads/2021/09/Cold-Strawberry-Cocktail-with-Fresh-Strawberries-and-Mint.jpg" alt="" />
-		</a>
-		<div className="caption">
-			<h3>Strawberry Mint</h3>
-			<p>So Fresh and So Clean</p>
-			
-		</div>
-	</article>
-	<article className='p-2'>
-		<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-			<img className="project-img rounded" src="https://hips.hearstapps.com/del.h-cdn.co/assets/15/38/1442432590-delish-halloween-cocktails-sweet-poison.png" alt="" />
-		</a>
-		<div className="caption">
-			<h3>Spooky Sweet</h3>
-			<p>Don't get too scared </p>
-		</div>
-	</article>
-	<article className='p-2'>
-		<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-			<img className="project-img rounded" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-191017-brunch-punch-0210-landscape-pf-1649601124.jpg?crop=0.673xw:1.00xh;0.228xw,0&resize=640:*" alt="" />
-		</a>
-		<div className="caption">
-			<h3>Brunch Punch</h3>
-			<p>Start your morning off right</p>
-		</div>
-	</article>
-	<article className='p-2'>
-		<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-			<img className="project-img rounded" src="https://recipetineats.com/wp-content/uploads/2019/10/Cosmopolitan-cocktail_2.jpg" alt="" />
-		</a>
-		<div className="caption">
-			<h3>Cosmopolitan</h3>
-			<p>Its out of this world </p>
-			<ul className="actions fixed">
-				<li><span className="button small">Details</span></li>
-			</ul>
-		</div>
-	</article>
-	<article className='p-2'>
-		<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-			<img className="project-img rounded" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/w-punta-de-mita-piwiwi-cocktail-preview-1548176063.jpeg?crop=1xw:1xh;center,top&resize=480:*" alt="" />
-		</a>
-		<div className="caption">
-			<h3>Punta de Mita</h3>
-			<p>Si se quedes</p>
-			
-		</div>
-	</article>
-	<article className='p-2'>
-		<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
-			<img className="project-img rounded" src="https://www.acouplecooks.com/wp-content/uploads/2020/03/Margarita-024.jpg" alt="" />
-		</a>
-		<div className="caption">
-			<h3>Original Margarita</h3>
-			<p>Quedes Mas</p>
-			<ul className="actions fixed">
-				<li><span className="button small">Details</span></li>
-			</ul>
-		</div>
-	</article>
-</div>
+
+
+				<article className='p-2'>
+					<a href="src/assets/Images/bubble-gum-colada.webp" className="image">
+						<img className="project-img rounded" src="https://tipsybartender.com/wp-content/uploads/2018/02/bubble-gum-colada.jpg" alt="" />
+					</a>
+					<div className="caption">
+						<h3>Bubble Gum Colada</h3>
+						<p>Taste the exciting flavors of this Cotton Candy drink</p>
+					</div>
+				</article>
+				<article className='p-2'>
+					<a href="images/wayfinder.png" className="image">
+						<img className="project-img rounded" src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/06/sex-on-the-beach-cocktail-recipe.jpg" alt="" />
+					</a>
+					<div className="caption">
+						<h3>Sex on The Beach</h3>
+						<p>No Description needed</p>
+
+					</div>
+				</article>
+				<article className='p-2'>
+					<a href="images/Keepr.png" className="image">
+						<img className="project-img rounded" src="https://images.immediate.co.uk/production/volatile/sites/2/2021/07/GettyImages_933801552-6e4a61e.jpg?quality=90&resize=700,466" alt="" />
+					</a>
+					<div className="caption">
+						<h3>Grapefruit Fresca</h3>
+						<p>All Strawberry fun</p>
+
+					</div>
+				</article>
+				<article className='p-2'>
+					<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
+						<img className="project-img rounded" src="https://insanelygoodrecipes.com/wp-content/uploads/2021/09/Cold-Strawberry-Cocktail-with-Fresh-Strawberries-and-Mint.jpg" alt="" />
+					</a>
+					<div className="caption">
+						<h3>Strawberry Mint</h3>
+						<p>So Fresh and So Clean</p>
+
+					</div>
+				</article>
+				<article className='p-2'>
+					<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
+						<img className="project-img rounded" src="https://hips.hearstapps.com/del.h-cdn.co/assets/15/38/1442432590-delish-halloween-cocktails-sweet-poison.png" alt="" />
+					</a>
+					<div className="caption">
+						<h3>Spooky Sweet</h3>
+						<p>Don't get too scared </p>
+					</div>
+				</article>
+				<article className='p-2'>
+					<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
+						<img className="project-img rounded" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-191017-brunch-punch-0210-landscape-pf-1649601124.jpg?crop=0.673xw:1.00xh;0.228xw,0&resize=640:*" alt="" />
+					</a>
+					<div className="caption">
+						<h3>Brunch Punch</h3>
+						<p>Start your morning off right</p>
+					</div>
+				</article>
+				<article className='p-2'>
+					<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
+						<img className="project-img rounded" src="https://recipetineats.com/wp-content/uploads/2019/10/Cosmopolitan-cocktail_2.jpg" alt="" />
+					</a>
+					<div className="caption">
+						<h3>Cosmopolitan</h3>
+						<p>Its out of this world </p>
+						<ul className="actions fixed">
+							<li><span className="button small">Details</span></li>
+						</ul>
+					</div>
+				</article>
+				<article className='p-2'>
+					<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
+						<img className="project-img rounded" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/w-punta-de-mita-piwiwi-cocktail-preview-1548176063.jpeg?crop=1xw:1xh;center,top&resize=480:*" alt="" />
+					</a>
+					<div className="caption">
+						<h3>Punta de Mita</h3>
+						<p>Si se quedes</p>
+
+					</div>
+				</article>
+				<article className='p-2'>
+					<a href="images/Screen Shot 2022-08-09 at 3.47.05 PM.png" className="image">
+						<img className="project-img rounded" src="https://www.acouplecooks.com/wp-content/uploads/2020/03/Margarita-024.jpg" alt="" />
+					</a>
+					<div className="caption">
+						<h3>Original Margarita</h3>
+						<p>Quedes Mas</p>
+						<ul className="actions fixed">
+							<li><span className="button small">Details</span></li>
+						</ul>
+					</div>
+				</article>
 			</div>
-      
+		</div>
 
-	
 
-		
-    )
-  }
+
+
+
+	)
 }
+
