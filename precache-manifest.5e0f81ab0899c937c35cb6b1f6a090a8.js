756d6ef5897f2ac33881c8488a16104dc0d1571d
Nik Lyons <nikolas.lyons23@gmail.com>
2022-09-14 11:12:59 -0600

    Updates

---
+++ precache-manifest.5e0f81ab0899c937c35cb6b1f6a090a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fb76c55af31403b7dbb",
+    "url": "/volumeone/static/css/main.79921d32.chunk.css"
+  },
+  {
+    "revision": "8fb76c55af31403b7dbb",
+    "url": "/volumeone/static/js/main.8fb76c55.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/volumeone/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "bb4554c69a47c9615691",
+    "url": "/volumeone/static/js/2.bb4554c6.chunk.js"
+  },
+  {
+    "revision": "a9df341753986d77eda5",
+    "url": "/volumeone/static/js/runtime~main.a9df3417.js"
+  },
+  {
+    "revision": "e22f4639dd172dd1db616fd66a3d10b1",
+    "url": "/volumeone/static/media/1-2-person-platter.e22f4639.png"
+  },
+  {
+    "revision": "e92d2a61c4f92cb05fe361044f9511c3",
+    "url": "/volumeone/static/media/4-7-person-platter.e92d2a61.png"
+  },
+  {
+    "revision": "7dfdc02509f68689631d1e535bbd24ee",
+    "url": "/volumeone/index.html"
+  }
+];
