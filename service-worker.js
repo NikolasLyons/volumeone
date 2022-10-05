cc748804aa1fd65300f7798f447aa3cca2f7fe8b
GitHub Action <github-action@users.noreply.github.com>
2022-10-05 00:17:49 +0000

    Deployed from NikolasLyons/volumeone@6804072c301b63d34bcdd9e4d73d7286b86c9576

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/volumeone/precache-manifest.a88d03a94d15f632949d0856a72417c6.js"
+  "/precache-manifest.3dd9e63b349e2dffd36ac019fa6bdfd4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/volumeone/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
